a6c10459a64f2cf951d11ac6e7873f11b9912919
Piotr <piotrmachnik@op.pl>
2021-10-07 08:19:57 +0200

    ert

---
+++ Assets/Scripts/ClockDigital.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+using System;
+
+public class ClockDigital : MonoBehaviour
+{
+    private Text textClock;
+
+    void Start()
+    {
+        textClock = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        string hour = LeadingZero(time.Hour);
+        string minute = LeadingZero(time.Minute);
+        string second = LeadingZero(time.Second);
+
+        textClock.text = hour + ":" + minute;
+    }
+
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
